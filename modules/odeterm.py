ee34c7d7a3e6cdb5cb6ebacb80b4e71555c93214
Neil Butcher <n.a.butcher@bham.ac.uk>
2025-05-08 11:27:35 +0100

    neatening

--- modules/odeterm.py
+++ modules/odeterm.py
@@ -1,11 +1,29 @@
-
-
 class SpeciesODETerm:
+    """
+    A single term in the ode function, or it's jacobian
+
+    It stores a list of contributions to a single function term, in the form of compiled, evals.
+    It constructs the full result by summing these at calltime.
+    """
+
     def __init__(self, contributions):
-        self.compiled_contributions = [compile(c,'<string>','eval') for c in contributions]
+        """
+        compiles the master array to be evaluated during integration
+
+        inputs:
+            contributions = list of strings which are compiled, then summed to give the full
+
+        """
+        self.compiled_contributions = [compile(c, '<string>', 'eval') for c in contributions]
 
     def __call__(self, full_dict):
+        """
+        Computes the numerical value of this term in the ODE equation (jacobian or function)
+
+        inputs:
+            full_dict = accumulated dictionary including concentrations, deposition rates and reaction rates
+        """
         result = 0.0
         for contribution in self.compiled_contributions:
-            result += eval(contribution,{},full_dict)
+            result += eval(contribution, {}, full_dict)
         return result
