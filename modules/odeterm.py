d41f5e7ea1fc743cd0cf2164d0a58b863f21d973
DrDaveShaw <david.shaw@york.ac.uk>
2025-05-09 11:08:37 +0100

    Added copyright to odeterm
    Updated copyright information for odeterm file as part of INCHEM-Py

--- modules/odeterm.py
+++ modules/odeterm.py
@@ -1,3 +1,31 @@
+"""
+Additional tools for importing and interpolating an input file of concentrations
+with time. Also for updating internal dictionaries with these concentrations.
+
+Copyright (C) 2019-2021 
+David Shaw : david.shaw@york.ac.uk
+Nicola Carslaw : nicola.carslaw@york.ac.uk
+
+All rights reserved.
+
+This file is part of INCHEM-Py.
+
+INCHEM-Py is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+INCHEM-Py is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with INCHEM-Py.  If not, see <https://www.gnu.org/licenses/>.
+
+File created by Neil Butcher
+"""
+
 class SpeciesODETerm:
     """
     A single term in the ode function, or it's jacobian
